86b1ccb3de566d39282a580095c9b7f9f7ba144e
ydb5755 <yisroel.d.baum@gmail.com>
2024-10-29 15:25:32 +0200

    move counter to global remove inputs from template and move all generation of inputs to js script

--- app/static/split_line.js
+++ app/static/split_line.js
@@ -1,25 +1,27 @@
 const addLineButton = document.getElementById("add-line-button");
 const lineContainer = document.getElementById('table-body');
+var counter = 1;
 
 function addLine(){
     const columns = ['Amount', 'Vendor', 'Confirmation Code', 'Note'];
     var tr = document.createElement('tr');
-    var counter = 2;
     columns.forEach(column => {
         var td = document.createElement('td');
         var ip = document.createElement('input');
         ip.type = 'text'
         ip.classList = 'form-control'
-        ip.name = `${column}-${counter++}`
+        ip.name = `${column}-${counter}`
         ip.placeholder = column
         td.appendChild(ip)
         tr.appendChild(td)
     })
+    counter++
     lineContainer.appendChild(tr)
     
 }
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    addLine()
     addLineButton.addEventListener('click', addLine)
 })
